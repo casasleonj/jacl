5981ab3f9647e53a430f6c6670e9ef820c519a2e
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2018-12-10 16:46:41 -0500

    config som folders api/v1 and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  #..config activeadmin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  #...
+  namespace :api do
+    namespace :v1 do
+
+    end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
